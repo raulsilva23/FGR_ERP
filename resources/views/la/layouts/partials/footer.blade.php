d3cb9db20d7e1f128566d8d7fa947911397cbe98
Raul FGR <raulfgr2@gmail.com>
2018-08-17 14:00:06 -0300

    Segundo commit: mudanças no footer

--- resources/views/la/layouts/partials/footer.blade.php
+++ resources/views/la/layouts/partials/footer.blade.php
@@ -1,8 +1,8 @@
 @if(!isset($no_padding))
 <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        Powered by <a href="http://dwijitsolutions.com">Dwij IT Solutions</a>
-    </div>
-    <strong>Copyright &copy; 2016
+      <div class="pull-right hidden-xs" >
+        <a href="http://fgr.eng.br">FGR Arquitetura e Engenharia</a>
+       <strong>Copyright &copy; 2018
+    	</div>
 </footer>
 @endif
